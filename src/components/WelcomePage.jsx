772f4bd88f82ac900fbdf2118471c1e26ea55ec3
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-04 23:06:41 +0200

    Create welcom.json

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -3,10 +3,19 @@ import "../styles/welcomePage.sass";
 import Welcome from "../images/welcome.jpg";
 
 export default function WelcomePage() {
+  /*<img
+        src={require(`../pictures/homePage/${dish.dishImg}.jpg`)}
+        className="homepage-image"
+        alt={dish.altImg}
+      />*/
   return (
-    <div className="welcompage-content">
-      <h1>Welcome</h1>
-      <img src={Welcome} />
+    <div>
+      <div className="welcompage-big-img ">
+        <h1>Welcome</h1>
+      </div>
+      <p>
+        Here you will find a lot of information abourt artist of different eras
+      </p>
     </div>
   );
 }
